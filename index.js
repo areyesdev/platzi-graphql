9df01a14e1cb5ba6d88c012d0740cfa4291103e2
Andres Reyes <ar97yf@gmail.com>
2020-12-28 20:13:20 -0500

    [ADD] env example

---
+++ index.js
@@ -0,0 +1,29 @@
+const { makeExecutableSchema } = require("graphql-tools");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
+
+const app = express();
+const port = process.env.port || 3000;
+
+// definimos el esquema
+const typeDefs = readFileSync(
+  join(__dirname, "lib", "schema.graphql"),
+  "utf-8"
+);
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+app.use(
+  "/api",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`);
+});
